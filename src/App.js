c76d9696400c10b9bc815a5062baccde1dcc23ac
Amber-Pittman <amber@amberpittman.com>
2020-01-09 21:11:30 -0500

    Working on the app

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const mapStateToProps = (state) => {
   return {
     additionalPrice: state.additionalPrice,
     car: state.car,
-    additionalFeatures: state.additionalFeatures
+    additionalFeatures: state.additionalFeatures,
   };
 }
 
